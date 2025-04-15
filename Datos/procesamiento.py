dfd54e7602b48527d0d2ba2ecacfb323845324f9
Nicolas <nabermedo@uc.cl>
2025-04-15 15:33:50 -0400

    Pre-procesamiento de los datos

--- Datos/procesamiento.py
+++ Datos/procesamiento.py
@@ -10,14 +10,12 @@ df.columns = df.columns.str.replace("Nan", " ", regex=False)
 # Reemplazar "Nan" en los valores del DataFrame
 df = df.replace(r"Nan", " ", regex=True)
 
-# Seleccionar las columnas que deseas mantener
+# Seleccionar las columnas que se desea mantener
 columnas_interes = ['Sr.no', 'Year', 'Mo', 'Earthquake Magnitude', 'Country', 'Location Name', 'Latitude', 'Longitude', 'Maximum Water Height (m)']
 df_filtrado = df[columnas_interes]
 
-# Eliminar columnas completamente vacías (si las hay)
+# Eliminar columnas completamente vacías
 df_filtrado = df_filtrado.dropna(how='all', axis=1)
 
 # Guardar el nuevo archivo CSV con las columnas seleccionadas
 df_filtrado.to_csv("tsunamis_filtrados.csv", index=False)
-
-print("Archivo procesado y guardado como 'tsunamis_filtrados.csv'")
